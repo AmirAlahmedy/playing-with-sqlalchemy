8ef0e1e4cfa55219978f9122709452780a6be489
Amir Alahmedy <ameer.alahmedy@gmail.com>
2024-06-24 22:22:51 +0300

    Adding up a foreign key with sqlalchemy ORM.

--- mysql-with-python/mysql-sqlalchemy-workspace/database.py
+++ mysql-with-python/mysql-sqlalchemy-workspace/database.py
@@ -1,6 +1,34 @@
-import sqlalchemy
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
+from sqlalchemy.orm import registry, relationship
 import os
 
 mysq_root_password = os.getenv("PASSWORD")
 
-engine = sqlalchemy.create_engine(f'mysql+mysqlconnector://root:{mysq_root_password}@localhost:3306/projects', echo=True)
+engine = create_engine(f'mysql+mysqlconnector://root:{mysq_root_password}@localhost:3306/projects', echo=True)
+
+mapper_registry = registry()
+
+Base = mapper_registry.generate_base()
+
+class Project(Base):
+    __tablename__ = 'projects'
+    project_id = Column(Integer, primary_key=True)
+    title = Column(String(length=50))
+    description = Column(String(length=50))
+
+    def __repr__(self):
+        return "<Project(title'{0}, description='{1}')>".format(self.title, self.description)
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    task_id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey('projects.project_id'))
+    description = Column(String(length=50))
+
+    project = relationship("Project")
+
+    def __repr__(self):
+        return "<Task(description='{0}')>".format(self.description)
+
+
+Base.metadata.create_all(engine)
